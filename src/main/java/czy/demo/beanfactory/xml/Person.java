9d4771b3bb5b8824d99057314c3f72755bef3e64
czy12345 <1253665625@qq.com>
2018-10-06 22:01:59 +0800

    XmlBeanFactory 示例

---
+++ src/main/java/czy/demo/beanfactory/xml/Person.java
@@ -0,0 +1,30 @@
+package czy.demo.beanfactory.xml;
+
+public class Person {
+
+
+    private String name;
+    private Integer age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+
+    public String toString(){
+        return "name="+name+",age="+age;
+    }
+
+}
--- src/main/java/czy/demo/beanfactory/xml/XmlBeanFactoryTest.java
+++ src/main/java/czy/demo/beanfactory/xml/XmlBeanFactoryTest.java
@@ -7,8 +7,10 @@ import org.springframework.core.io.Resource;
 public class XmlBeanFactoryTest {
 
     public static void main(String[] args){
-        Resource resource = new ClassPathResource("czy/demo/resource/beans.xml");
+        Resource resource = new ClassPathResource("czy/demo/beanfactory/xml/beans.xml");
         XmlBeanFactory factory = new XmlBeanFactory(resource);
+        Person person  = factory.getBean(Person.class);
+        System.out.println(person);
     }
 
 }
