5f6e465b98dc22bddef59c58cfef1fe877fea967
陈振远 <1253665625@qq.com>
2019-05-16 12:01:35 +0800

    测试

--- spring-core-demo/src/test/java/czy/demo/resource/ClassPathResourceTest.java
+++ spring-core-demo/src/test/java/czy/demo/resource/ClassPathResourceTest.java
@@ -1,16 +1,19 @@
 package czy.demo.resource;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.core.io.ClassPathResource;
 
+import java.io.IOException;
 import java.io.InputStream;
-
 
 public class ClassPathResourceTest {
 
-    public static void main(String[] args)throws Exception{
-
-        /* 可以直接加载打包进类路径的资源文件 */
-        ClassPathResource resource = new ClassPathResource("czy/demo/resource/beans.xml");
+    @Test
+    @DisplayName("ClassPathResource简单测试")
+    public void simple()throws IOException {
+        /* 直接加载类路径的资源文件，资源文件不需要在test目录下 */
+        ClassPathResource resource = new ClassPathResource("db.properties");
         System.out.println("资源路径为: "+resource.getPath());
         System.out.println("资源是否存在: "+resource.exists());
         System.out.println("资源内容为: ");
@@ -21,5 +24,6 @@ public class ClassPathResourceTest {
             System.out.println(new String(bytes,0,length));
         }
     }
+
 
 }
