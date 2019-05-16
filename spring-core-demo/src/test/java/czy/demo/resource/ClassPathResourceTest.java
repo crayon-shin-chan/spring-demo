7bc79773de6a176aee8c607f2165a848ff8dfba5
陈振远 <1253665625@qq.com>
2019-05-16 14:21:31 +0800

    文件系统资源类测试

--- spring-core-demo/src/test/java/czy/demo/resource/ClassPathResourceTest.java
+++ spring-core-demo/src/test/java/czy/demo/resource/ClassPathResourceTest.java
@@ -1,29 +1,17 @@
 package czy.demo.resource;
 
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.core.io.ClassPathResource;
-
+import static org.junit.jupiter.api.Assertions.*;
 import java.io.IOException;
-import java.io.InputStream;
 
 public class ClassPathResourceTest {
 
     @Test
-    @DisplayName("ClassPathResource简单测试")
     public void simple()throws IOException {
-        /* 直接加载类路径的资源文件，资源文件不需要在test目录下 */
         ClassPathResource resource = new ClassPathResource("db.properties");
-        System.out.println("资源路径为: "+resource.getPath());
-        System.out.println("资源是否存在: "+resource.exists());
-        System.out.println("资源内容为: ");
-        InputStream in = resource.getInputStream();
-        byte[] bytes = new byte[1048];
-        int length = 0;
-        while((length = in.read(bytes))!=-1){
-            System.out.println(new String(bytes,0,length));
-        }
+        assertEquals("db.properties",resource.getPath(),"资源路径不正确");
+        assertTrue(resource.exists(),"资源不存在");
     }
-
 
 }
--- spring-core-demo/src/test/java/czy/demo/resource/FileSystemResourceTest.java
+++ spring-core-demo/src/test/java/czy/demo/resource/FileSystemResourceTest.java
@@ -1,30 +1,28 @@
 package czy.demo.resource;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.Resource;
 import org.springframework.util.ResourceUtils;
+import static org.junit.jupiter.api.Assertions.*;
 
-import java.io.InputStream;
+import java.io.IOException;
 import java.net.URLDecoder;
 
 
 public class FileSystemResourceTest {
 
-    public static void main(String[] args)throws Exception{
-
+    @Test
+    public void simple()throws IOException {
         /* spring的ResourceUtils可以直接获取类路径下文件的绝对路径,需要URL解码 */
-        String path = ResourceUtils.getURL("classpath:czy/demo/resource/beans.xml").getPath();
-        System.out.println(URLDecoder.decode(path));
-        Resource resource = new FileSystemResource(URLDecoder.decode(path));
-        System.out.println("资源是否存在: "+resource.exists());
-        System.out.println("资源路径为: "+((FileSystemResource) resource).getPath());
-        System.out.println("资源内容为: ");
-        InputStream in = resource.getInputStream();
-        byte[] bytes = new byte[1048];
-        int length = 0;
-        while((length = in.read(bytes))!=-1){
-            System.out.println(new String(bytes,0,length));
-        }
+        String path = ResourceUtils.getURL("classpath:db.properties").getPath();
+        Resource resource = new FileSystemResource(path);
+        assertTrue(resource.exists(),"资源不存在");
+        assertTrue(resource.isFile(),"资源不是文件");
+        assertFalse(resource.isOpen(),"资源已经打开");
+        assertTrue(resource.isReadable(),"资源不可读");
+        assertTrue(((FileSystemResource) resource).isWritable(),"资源不可写");
+        assertEquals("db.properties",resource.getFilename(),"文件名称不正确");
     }
 
 }
