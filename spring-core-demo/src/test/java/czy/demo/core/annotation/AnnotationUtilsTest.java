5f6187dbd1de46ed1cb7ea21701dfad473ce7480
陈振远 <1253665625@qq.com>
2019-05-16 14:51:30 +0800

    注解工具测试类

--- spring-core-demo/src/test/java/czy/demo/core/annotation/AnnotationUtilsTest.java
+++ spring-core-demo/src/test/java/czy/demo/core/annotation/AnnotationUtilsTest.java
@@ -3,6 +3,9 @@ package czy.demo.core.annotation;
 import org.junit.jupiter.api.Test;
 import org.springframework.core.annotation.AnnotationUtils;
 import org.springframework.core.annotation.Order;
+
+import java.lang.annotation.Annotation;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 /* AnnotationUtils注解工具类测试 */
@@ -16,6 +19,11 @@ public class AnnotationUtilsTest {
 
         Order methodOrder = AnnotationUtils.findAnnotation(AnnotationUtilsMain.class.getMethod("order"),Order.class);
         assertEquals(100,methodOrder.value(),"方法序号不是100");
+
+        /* 这里获取到的注解对象为代理 */
+        Annotation[] annotations = AnnotationUtils.getAnnotations(AnnotationUtilsMain.class);
+        assertEquals(1,annotations.length,"类上注解不止一个");
+
     }
 
 }
